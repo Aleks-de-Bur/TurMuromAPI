461e777bdd3da74f53c1ced8310e45cb6389c365
Aleks-de-Bur <Aleks-de-Bur@yandex.ru>
2023-04-04 15:23:23 +0300

    Update

--- src/main/java/com/alexdebur/TurMurom/Controllers/MapController.java
+++ src/main/java/com/alexdebur/TurMurom/Controllers/MapController.java
@@ -5,12 +5,17 @@ import com.alexdebur.TurMurom.Services.CategoryService;
 import com.alexdebur.TurMurom.Services.MarkPhotoService;
 import com.alexdebur.TurMurom.Services.MarkService;
 import com.alexdebur.TurMurom.Services.ScheduleService;
+import com.alexdebur.TurMurom.WorkClasses.InteractionPhoto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
 import java.util.List;
+
+import static com.alexdebur.TurMurom.Controllers.MarkController.UPLOAD_DIRECTORY;
 
 @Controller
 public class MapController {
@@ -25,6 +30,17 @@ public class MapController {
     @GetMapping("/map")
     public String getMap(Model model){
         List<Mark> allMarks = markService.getAllMarks();
+
+        for (var mark : allMarks){
+            for (var photo : mark.getMarkPhotos()){
+                try {
+                    photo.setPathPhoto(InteractionPhoto.getPhoto(UPLOAD_DIRECTORY + photo.getPathPhoto()));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
 
         model.addAttribute("marks", allMarks);
         model.addAttribute("activePage", "map");
